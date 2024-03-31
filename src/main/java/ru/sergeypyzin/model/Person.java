68c4d7c7d49c4470faa205f7d5ab6b2c42ad0cb3
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-31 10:22:32 +0300

    changed pom.xml and Person as added project lombok

--- src/main/java/ru/sergeypyzin/model/Person.java
+++ src/main/java/ru/sergeypyzin/model/Person.java
@@ -1,10 +1,19 @@
 package ru.sergeypyzin.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     private String firstName;
@@ -12,40 +21,6 @@ public class Person {
     private String lastName;
 
     private int age;
-
-
-    public Person() {
-    }
-
-    public Person(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
