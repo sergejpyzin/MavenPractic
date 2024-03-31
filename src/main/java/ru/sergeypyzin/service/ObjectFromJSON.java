51aa5862cfc24145c25775728512f4dcc1bba094
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-31 09:59:00 +0300

    create ObjectFromJSON

---
+++ src/main/java/ru/sergeypyzin/service/ObjectFromJSON.java
@@ -0,0 +1,15 @@
+package ru.sergeypyzin.service;
+
+import com.google.gson.Gson;
+import ru.sergeypyzin.model.Person;
+
+public class ObjectFromJSON{
+
+    static Gson gson = new Gson();
+
+
+    public static Person createFromJSON(String jsonString) {
+
+        return gson.fromJson(jsonString, Person.class);
+    }
+}
