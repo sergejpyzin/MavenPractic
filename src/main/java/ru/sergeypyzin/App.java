7e053959549488079bfeecea8783ee009ac2ec3f
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-03-31 11:35:57 +0300

    added commits to all classes

--- src/main/java/ru/sergeypyzin/App.java
+++ src/main/java/ru/sergeypyzin/App.java
@@ -7,16 +7,22 @@ import ru.sergeypyzin.service.PersonToJSON;
 public class App {
     public static void main(String[] args) {
 
+        // создаем экземпляр класса Person
         Person person = new Person("Sergey", "Sergeev", 42);
 
+        // Преобразуем экземпляр класса Person в json-строку
         String jsonString = PersonToJSON.createJSONString(person);
 
+        // Выводим на печать json-строку
         System.out.println(jsonString);
 
+        // Создаем новую json-строку
         String newJsonString = "{\"firstName\":\"Ivan\",\"lastName\":\"Ivanov\",\"age\":30}";
 
+        // Создаем экземпляр класса из json-строки
         Person newPerson = PersonFromJSON.createFromJSON(newJsonString);
 
+        // Выводим на печать экземпляр класса Person
         System.out.println(newPerson);
     }
 }
--- src/main/java/ru/sergeypyzin/service/PersonToJSON.java
+++ src/main/java/ru/sergeypyzin/service/PersonToJSON.java
@@ -7,6 +7,22 @@ public class PersonToJSON {
 
     private static final Gson gson = new Gson();
 
+
+    /**
+     * Метод преобразования объекта типа Person в строку JSON с использованием библиотеки Gson.
+     * - Метод toJson() библиотеки Gson принимает объект типа Person и преобразует его в строку JSON.
+     * - Эта JSON-строка содержит поля объекта Person в формате ключ-значение, соответствующем структуре класса Person.
+     * Например, если класс Person имеет поля firstName, lastName и age, то возвращенная JSON-строка будет выглядеть примерно так:
+     * {
+     *   "firstName": "John",
+     *   "lastName": "Doe",
+     *   "age": 30
+     * }
+     *
+     * @param person объект класса Person
+     *
+     * @return преобразованный в JSON-строку объект класса Person
+     */
     public static String createJSONString (Person person){
         return gson.toJson(person);
     }
